e997da57ddb26750304eb687d82087216c95d6d9
Ivo Yueh <bitxwise@gmail.com>
2018-11-20 18:41:33 -0700

    Added GET to vehicles API

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -69,5 +69,18 @@ namespace vega.Controllers
 
             return Ok(id);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await this.context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            
+            if(vehicle == null)
+                return NotFound();
+            
+            var result = this.mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
     }
 }
